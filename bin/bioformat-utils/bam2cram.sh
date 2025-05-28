9786ac54e695c1f771822990cb0cef56b50386be
Rafiga Masmaliyeva <47282203+RafigaM@users.noreply.github.com>
2025-05-28 16:36:59 +0200

    Update bam2cram.sh

--- bin/bioformat-utils/bam2cram.sh
+++ bin/bioformat-utils/bam2cram.sh
@@ -12,14 +12,15 @@ if [ ! -d "$output_dir" ]; then
     mkdir -p "$output_dir" || { echo "Failed to create output directory: $output_dir"; exit 1; }
 fi
 
-convert_to_bam() {
+convert_to_cram() {
     bam_file="$1"
     output_dir="$2"
-    out_file="$output_dir"/"$(basename "${bam_file%.cram}.bam")"
-    samtools view -T /fs/s6k/groups/agmedgen/KOL_UOL/reference_GRCh38/Homo_sapiens/GATK/GRCh38/Sequence/WholeGenomeFasta/Homo_sapiens_assembly38.fasta -b -o "$out_file" "$bam_file"
+    out_file="$output_dir"/"$(basename "${bam_file%.bam}.cram")"
+    samtools view -C -T /fs/s6k/groups/agmedgen/KOL_UOL/reference_GRCh38/Homo_sapiens/GATK/GRCh38/Sequence/WholeGenomeFasta/Homo_sapiens_assembly38.fasta -o "$out_file" "$bam_file"
     echo "Converted $bam_file to $out_file"
 }
 
-export -f convert_to_bam
+export -f convert_to_cram
 
-parallel -a "$bam_list" -j8 convert_to_bam {} "$output_dir"
+parallel -a "$bam_list" -j8 convert_to_cram {} "$output_dir"
+~                                                            
